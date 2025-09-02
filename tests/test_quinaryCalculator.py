d8f3069dd0e84abbf559a3fc259ac719d0e081af
Brandon Dean <101386937+Mrpoink@users.noreply.github.com>
2025-09-02 16:17:15 -0400

    Update test_quinaryCalculator.py
    created callables

--- tests/test_quinaryCalculator.py
+++ tests/test_quinaryCalculator.py
@@ -4,28 +4,28 @@ from src import quinaryCalculator as gc
 class TestQuinaryCalculatorOperations(unittest.TestCase):
 
     def test_calculatorAddition(self):
-        self.assertEqual(30, gc("12 + 3"))
+        self.assertEqual(30, gc.quinaryCalculator("12 + 3"))
 
     def test_calculatorDivisionONE(self):
-        self.assertEqual(33.0, gc("432/24"))
+        self.assertEqual(33.0, gc.quinaryCalculator("432/24"))
 
     def test_calculatorDivisionZERO(self):
-        self.assertEqual(0.0404, gc("1/6"))
+        self.assertEqual(0.0404, gc.quinaryCalculator("1/6"))
 
     def test_calculatorSubtractPOS(self):
-        self.assertEqual(30, gc("27 - 12"))
+        self.assertEqual(30, gc.quinaryCalculator("27 - 12"))
 
     def test_calculatorSubtractNEG(self):
-        self.assertEqual(-30, gc("12 - 27"))
+        self.assertEqual(-30, gc.quinaryCalculator("12 - 27"))
 
     def test_calculatorMultiply(self):
-        self.assertEqual(11, gc("1 * 6"))
+        self.assertEqual(11, gc.quinaryCalculator("1 * 6"))
 
     def test_calculatorSquare(self):
-        self.assertEqual(4104, gc("23^2"))
+        self.assertEqual(4104, gc.quinaryCalculator("23^2"))
 
     def test_calculatorSquareRoot(self):
-        self.assertEqual(10, gc("√25"))
+        self.assertEqual(10, gc.quinaryCalculator("√25"))
 
 
 if __name__ == "__main__":

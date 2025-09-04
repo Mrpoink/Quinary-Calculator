7edecc3a55de8943943cfca3084caa562878bf03
Brandon Dean <101386937+Mrpoink@users.noreply.github.com>
2025-09-03 22:25:22 -0400

    Update quinaryCalculator.py (#9)
    * Update quinaryCalculator.py
    
    fixed string issues and made compatible with tests
    
    * Update test_quinaryCalculator.py
    
    fixed test cases

--- tests/test_quinaryCalculator.py
+++ tests/test_quinaryCalculator.py
@@ -4,28 +4,34 @@ from src import quinaryCalculator as gc
 class TestQuinaryCalculatorOperations(unittest.TestCase):
 
     def test_calculatorAddition(self):
-        self.assertEqual(20, gc.quinaryCalculator("12 + 3"))
+        self.assertEqual('20', gc("12 + 3"))
 
     def test_calculatorDivisionONE(self):
-        self.assertEqual(33.0, gc.quinaryCalculator("432/24"))
+        self.assertEqual('4', gc("4/1"))
 
     def test_calculatorDivisionZERO(self):
-        self.assertEqual(0.0404, gc.quinaryCalculator("1/6"))
+        self.assertEqual('0', gc("1/4"))
 
     def test_calculatorSubtractPOS(self):
-        self.assertEqual(30, gc.quinaryCalculator("27 - 12"))
+        self.assertEqual('1', gc("2 - 1"))
 
     def test_calculatorSubtractNEG(self):
-        self.assertEqual(-30, gc.quinaryCalculator("12 - 27"))
+        self.assertEqual('-1', gc("1 - 2"))
+
+    def test_calculatorSubtractPOS(self):
+        self.assertEqual('2', gc("3 - 1"))
+
+    def test_calculatorSubtractNEG(self):
+        self.assertEqual('-2', gc("1 - 3"))
 
     def test_calculatorMultiply(self):
-        self.assertEqual(11, gc.quinaryCalculator("1 * 6"))
+        self.assertEqual('4', gc("1 * 4"))
 
     def test_calculatorSquare(self):
-        self.assertEqual(4104, gc.quinaryCalculator("23^2"))
+        self.assertEqual('1034', gc("22^2"))
 
     def test_calculatorSquareRoot(self):
-        self.assertEqual(10, gc.quinaryCalculator("√25"))
+        self.assertEqual('2', gc("√4"))
 
 
 if __name__ == "__main__":

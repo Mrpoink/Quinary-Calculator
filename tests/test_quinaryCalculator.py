88659c7a00151beeb182a922d9aa4d01b12f10bc
Brandon Dean <101386937+Mrpoink@users.noreply.github.com>
2025-09-03 22:48:55 -0400

    Update test_quinaryCalculator.py
    fixed integer string values

--- tests/test_quinaryCalculator.py
+++ tests/test_quinaryCalculator.py
@@ -4,34 +4,34 @@ from src import quinaryCalculator as gc
 class TestQuinaryCalculatorOperations(unittest.TestCase):
 
     def test_calculatorAddition(self):
-        self.assertEqual('20', gc.quinaryCalculator("12 + 3"))
+        self.assertEqual('\20, gc.quinaryCalculator("12 + 3"))
 
     def test_calculatorDivisionONE(self):
-        self.assertEqual('4', gc.quinaryCalculator("4/1"))
+        self.assertEqual(4, gc.quinaryCalculator("4/1"))
 
     def test_calculatorDivisionZERO(self):
-        self.assertEqual('0', gc.quinaryCalculator("1/4"))
+        self.assertEqual(0, gc.quinaryCalculator("1/4"))
 
     def test_calculatorSubtractPOS(self):
-        self.assertEqual('1', gc.quinaryCalculator("2 - 1"))
+        self.assertEqual(1, gc.quinaryCalculator("2 - 1"))
 
     def test_calculatorSubtractNEG(self):
-        self.assertEqual('-1', gc.quinaryCalculator("1 - 2"))
+        self.assertEqual(-1, gc.quinaryCalculator("1 - 2"))
 
     def test_calculatorSubtractPOS(self):
-        self.assertEqual('2', gc.quinaryCalculator("3 - 1"))
+        self.assertEqual(2, gc.quinaryCalculator("3 - 1"))
 
     def test_calculatorSubtractNEG(self):
-        self.assertEqual('-2', gc.quinaryCalculator("1 - 3"))
+        self.assertEqual(-2, gc.quinaryCalculator("1 - 3"))
 
     def test_calculatorMultiply(self):
-        self.assertEqual('4', gc.quinaryCalculator("1 * 4"))
+        self.assertEqual(4, gc.quinaryCalculator("1 * 4"))
 
     def test_calculatorSquare(self):
-        self.assertEqual('1034', gc.quinaryCalculator("22^2"))
+        self.assertEqual(1034, gc.quinaryCalculator("22^2"))
 
     def test_calculatorSquareRoot(self):
-        self.assertEqual('2', gc.quinaryCalculator("√4"))
+        self.assertEqual(2, gc.quinaryCalculator("√4"))
 
 
 if __name__ == "__main__":
